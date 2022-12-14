7ba0bfc542b51bd92319fab8e73b9f738da3d3d3
Scott Smith <ssmith@tmvse.com>
2022-12-14 18:26:56 +0000

    day14 tidy

--- src/day14/day14.cpp
+++ src/day14/day14.cpp
@@ -5,26 +5,20 @@
 #include <algorithm>
 #include <numeric>
 #include <functional>
-#include <assert.h>
+#include <unordered_map>
 
 struct pos_t{
     int x, y;
 };
 
-pos_t operator-(const pos_t& a,const pos_t& b){
-    return { a.x - b.x, a.y - b.y };
-}
+struct pos_hash{
+    std::size_t operator() (const pos_t& pos) const {
+        return std::hash<int>()(pos.x) ^ std::hash<int>()(pos.y);
+    }
+};
 
-pos_t operator+(const pos_t& a,const pos_t& b){
-    return { a.x + b.x, a.y + b.y };
-}
-
-pos_t minimum(const pos_t& a,const pos_t& b){
-    return { std::min(a.x, b.x), std::min(a.y, b.y) };
-}
-
-pos_t maximum(const pos_t& a,const pos_t& b){
-    return { std::max(a.x, b.x), std::max(a.y, b.y) };
+bool operator==(const pos_t& a,const pos_t& b){
+    return std::make_tuple(a.x,a.y) == std::make_tuple(b.x,b.y);
 }
 
 using path_t = std::vector<pos_t>;
@@ -59,27 +53,26 @@ paths_t load_input(const std::string& file){
 }
 
 struct grid_t{
-    std::vector<char> data;
-    int width;
-    int height;
+    std::unordered_map<pos_t,char,pos_hash> points;
+    int max_height;
+    bool has_floor;
 
     char& operator()(int x,int y){
-        return data[y*width+x];
+        return points[{x,y}];
+    }
+
+    char operator()(int x,int y) const{
+        if(!points.count({x,y})){
+            return '.';
+        }
+        return points.at({x,y});
     }
 };
 
-void print(const grid_t& grid){
-    std::cout << std::endl;
-    for(int y=0; y<grid.height; ++y){
-        for(int x=0; x<grid.width; ++x){
-            std::cout << grid.data[y*grid.width+x];
-        }
-        std::cout << std::endl;
+bool traversable(const grid_t& grid, int x, int y){
+    if(grid.has_floor && y >= grid.max_height){
+        return false;
     }
-    std::cout << std::endl;
-}
-
-bool traversable(grid_t& grid, int x, int y){
     return grid(x,y) != '#' && grid(x,y) != 'o';
 }
 
@@ -88,23 +81,26 @@ bool drop_sand(grid_t& grid, int x, int y)
     bool at_rest = false;
     while(!at_rest)
     {
-        if(y+1 >= grid.height-1){
-            break;
+        if(!grid.has_floor && y+1 > grid.max_height){
+            break; // in the void
         }
 
-        while(traversable(grid,x,y+1)){ 
-            y++;
+        if(grid.has_floor && y+1 >= grid.max_height){
+            at_rest = true;
+            break; // hit the floor
         }
 
-        if(traversable(grid,x-1,y+1)){
-            x--;
-            y++;
+        while(traversable(grid, x, y+1)){ 
+            y++; // falling
+        }
+
+        if(traversable(grid, x-1, y+1)){
+            x--; y++; // move diagonal left
             continue;
         }
 
-        if(traversable(grid,x+1,y+1)){
-            x++;
-            y++;
+        if(traversable(grid, x+1, y+1)){
+            x++; y++; // move diagonal right
             continue;
         }
 
@@ -118,109 +114,69 @@ bool drop_sand(grid_t& grid, int x, int y)
     return at_rest;
 }
 
-
-auto part1(const paths_t& paths) 
-{  
-    pos_t start { 500, 0 };
-    pos_t min_pos = { INT_MAX, INT_MAX };
-    pos_t max_pos = { INT_MIN, INT_MIN };
-
-    min_pos = minimum(min_pos, start);
-    max_pos = maximum(max_pos, start);
-    for(auto& path : paths){
-        for(auto& pos : path){
-            min_pos = minimum(min_pos, pos);
-            max_pos = maximum(max_pos, pos);
-        }
-    }
-
-    min_pos = min_pos - pos_t{ 1, 1 };
-    max_pos = max_pos + pos_t{ 1, 1 };
-    pos_t dims = max_pos - min_pos + pos_t{ 1, 1 };
-    grid_t grid = { std::vector<char>(dims.x * dims.y, '.'), dims.x, dims.y };
+grid_t setup_grid(const paths_t& paths, const pos_t& start, bool has_floor)
+{
+    grid_t grid;
+    grid.max_height = INT_MIN;
+    grid.has_floor = has_floor;
 
     auto fill_line = [&](const pos_t& a, const pos_t& b){
         if(a.x == b.x){
             int ydir = a.y < b.y ? 1 : -1;
             for(int y=a.y; y!=b.y+ydir; y+=ydir){
-                grid(a.x-min_pos.x, y-min_pos.y) = '#';
+                grid(a.x, y) = '#';
             }
         }else if(a.y == b.y){
             int xdir = a.x < b.x ? 1 : -1;
             for(int x=a.x; x!=b.x+xdir; x+=xdir){
-                grid(x-min_pos.x, a.y-min_pos.y) = '#';
+                grid(x, a.y) = '#';
             }
         }
     };
 
-    grid(start.x-min_pos.x, start.y-min_pos.y) = '+';
+    grid(start.x, start.y) = '+';
     for(auto& path : paths){
         for(int i=0; i<path.size()-1; ++i){
-            fill_line(path[i],path[i+1]);
+            fill_line(path[i], path[i+1]);
+            grid.max_height = std::max(grid.max_height, path[i].y);
+            grid.max_height = std::max(grid.max_height, path[i+1].y);
         }
     }
 
-    while(drop_sand(grid, start.x-min_pos.x, 0)){
-
+    if(has_floor){
+        grid.max_height += 2;
     }
 
-    return std::count_if(grid.data.begin(), grid.data.end(), [](auto c){
-        return c == 'o';  
-    });
+    return grid;
+}
+
+auto count_sand(const grid_t& grid){
+   return std::count_if(grid.points.begin(), grid.points.end(), [](auto& pos_and_c){
+        return pos_and_c.second == 'o';  
+    }); 
+}
+
+auto part1(const paths_t& paths) 
+{  
+    pos_t start { 500, 0 };
+    grid_t grid = setup_grid(paths, start, false);
+
+    while(drop_sand(grid, start.x, start.y)){
+    }
+
+    return count_sand(grid);
 }
 
 auto part2(const paths_t& paths) 
 {  
     pos_t start { 500, 0 };
-    pos_t min_pos = { INT_MAX, INT_MAX };
-    pos_t max_pos = { INT_MIN, INT_MIN };
+    grid_t grid = setup_grid(paths, start, true);
 
-    min_pos = minimum(min_pos, start);
-    max_pos = maximum(max_pos, start);
-    for(auto& path : paths){
-        for(auto& pos : path){
-            min_pos = minimum(min_pos, pos);
-            max_pos = maximum(max_pos, pos);
-        }
+    while(grid(start.x, start.y) == '+'){
+        drop_sand(grid, start.x, start.y);
     }
 
-    max_pos = max_pos + pos_t{ 0, 2 };
-
-    min_pos = min_pos - pos_t{ 200, 1 };
-    max_pos = max_pos + pos_t{ 200, 1 };
-    pos_t dims = max_pos - min_pos + pos_t{ 1, 1 };
-    grid_t grid = { std::vector<char>(dims.x * dims.y, '.'), dims.x, dims.y };
-
-    auto fill_line = [&](const pos_t& a, const pos_t& b){
-        if(a.x == b.x){
-            int ydir = a.y < b.y ? 1 : -1;
-            for(int y=a.y; y!=b.y+ydir; y+=ydir){
-                grid(a.x-min_pos.x, y-min_pos.y) = '#';
-            }
-        }else if(a.y == b.y){
-            int xdir = a.x < b.x ? 1 : -1;
-            for(int x=a.x; x!=b.x+xdir; x+=xdir){
-                grid(x-min_pos.x, a.y-min_pos.y) = '#';
-            }
-        }
-    };
-
-    grid(start.x-min_pos.x, start.y-min_pos.y) = '+';
-    for(auto& path : paths){
-        for(int i=0; i<path.size()-1; ++i){
-            fill_line(path[i],path[i+1]);
-        }
-    }
-
-    fill_line({min_pos.x,max_pos.y-1},{max_pos.x,max_pos.y-1});
-
-    while(grid(start.x-min_pos.x, start.y-min_pos.y) == '+'){
-        drop_sand(grid, start.x-min_pos.x, 0);
-    }
-
-    return std::count_if(grid.data.begin(), grid.data.end(), [](auto c){
-        return c == 'o';  
-    });
+    return count_sand(grid);
 }
 
 void main()
@@ -228,17 +184,9 @@ void main()
     auto test_values = load_input("../src/day14/test_input.txt");
     auto actual_values = load_input("../src/day14/input.txt");
 
-    /*std::cout << "part1: " << part1(test_values) << std::endl;
+    std::cout << "part1: " << part1(test_values) << std::endl;
     std::cout << "part1: " << part1(actual_values) << std::endl;
 
     std::cout << "part2: " << part2(test_values) << std::endl;
-    std::cout << "part2: " << part2(actual_values) << std::endl;*/
-
-    assert(part1(test_values) == 24);
-    assert(part1(actual_values) == 763);
-
-    assert(part2(test_values) == 93);
-    assert(part2(actual_values) == 23921);
-
-    std::cout << "PAASSASSASASSSSS!" << std::endl;
+    std::cout << "part2: " << part2(actual_values) << std::endl;
 }
